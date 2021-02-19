b36aa8c6ee54bec5d36100673c361a845971babd
lesson <lisen@qbyun.com>
2021-02-19 17:26:02 +0800

    redux 的compose 解析

--- javascript/closure/compose.js
+++ javascript/closure/compose.js
@@ -39,27 +39,55 @@
 // }
 
 
-
 // 它接受任意多个函数作为参数（这些函数都只接受一个参数），然后compose返回的也是一个函数，达到以下的效果：
 
 // 1 柯里化 思想
-const compose = (...funcs)=> x => {
-  let len = funcs.length
-  if(len == 0) return x
-  if(len == 1) return  funcs[0](x)
-  return  funcs.reduceRight((result, item)=> {
-    return item(result)
-  },x)
-
-}
+// const compose = (...funcs)=> x => {
+//   let len = funcs.length
+//   if(len == 0) return x
+//   if(len == 1) return  funcs[0](x)
+//   return  funcs.reduceRight((result, item)=> {
+//     console.log(x, 'x',)
+//     return item(result)
+//   },x)
+//
+// }
 
 const add1 = (x) => x + 1;
 const mul3 = (x) => x * 3;
 const div2 = (x) => x / 2;
 div2(mul3(add1(add1(0)))); //=>3
-const operate = compose(div2, mul3, add1, add1);
-console.log(operate(0));
-console.log(operate(2));
-console.log(compose()(10));
+// const operate = compose(div2, mul3, add1, add1);
+// console.log(operate(0));
+// console.log(operate(2));
+// console.log(compose()(10));
 
 // 2 . redux 源码处理
+function compose(...funcs) {
+  if (funcs.length === 0) {
+    // infer the argument type so it is usable in inference down the line
+    return arg => {
+      console.log(arg, 'arg');
+      return arg
+    }
+  }
+  if (funcs.length === 1) {
+    console.log(funcs[0].toString())
+    return funcs[0] // 函数返回出来了
+    // return ~function (x) {
+    //   return x +1
+    // }()
+
+  }
+  return funcs.reduce((prev, current) => (...args) => prev(current(...args)))
+  // return funcs.reduce(function (prev, current) {
+  //   return function (...args) {
+  //     console.log(prev.toString(), 'prev')
+  //     console.log(current.toString(), 'current')
+  //     return prev(current(...args))
+  //   }
+  // })
+}
+
+// console.log(compose(div2, mul3, add1)(2));
+// console.log(compose(add1)(2));
