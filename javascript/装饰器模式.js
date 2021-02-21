ed69ea0ef70436eaa15824810b92469e89bddd51
lesson <1565510080@qq.com>
2021-02-21 18:51:54 +0800

    prettier 失效

--- javascript/装饰器模式.js
+++ javascript/装饰器模式.js
@@ -4,73 +4,81 @@
 // 1) 包装类 包装类中的属性 类中的方法
 // 装饰类  ：可以在类上 扩展一些方法 或者属性
 @add
-class My{
-    @enumerable PI = 3.14; // 不可枚举
-    @readonly r = 3.15
+class My {
+  @enumerable PI = 3.14; // 不可枚举
+  @readonly r = 3.15
 }
-function add(target){
-    target.type = 'my'
+
+function add(target) {
+  target.type = 'my'
 }
-function readonly(target,key,descriptor){// Object.defineproperty(obj,pi,{})
-    // console.log(target,key,descriptor.initializer());
-    descriptor.writable = false;
-    // setTimeout(()=>{
-    //     console.log(My.prototype === target);
-    // })
+
+function readonly(target, key, descriptor) {// Object.defineproperty(obj,pi,{})
+  // console.log(target,key,descriptor.initializer());
+  descriptor.writable = false;
+  // setTimeout(()=>{
+  //     console.log(My.prototype === target);
+  // })
 }
-function enumerable(target,key,descriptor){
-    console.log(descriptor.enumerable)
-    descriptor.enumerable = false;
+
+function enumerable(target, key, descriptor) {
+  console.log(descriptor.enumerable)
+  descriptor.enumerable = false;
 }
+
 let my = new My();
-for(let key in my){
-    console.log(key);
+for (let key in my) {
+  console.log(key);
 }
 
 
+class Animal {
+  @enumerable
+  @readonly PI = 3.4
 
-class Animal{
-    @enumerable
-    @readonly PI = 3.4
-    @beforeSay
-    say(){
-        console.log('say')
-    }
+  @beforeSay
+  say() {
+    console.log('say')
+  }
 }
 
-function beforeSay(target,key,descriptor){
-    let oldSay = descriptor.value
-    descriptor.value = function(){
-        console.log('before say')
-        oldSay.call(this)
-    }
+function beforeSay(target, key, descriptor) {
+  let oldSay = descriptor.value
+  descriptor.value = function () {
+    console.log('before say')
+    oldSay.call(this)
+  }
 }
+
 let animal = new Animal();
-for(let key in animal){
-    if(animal.hasOwnProperty(key)) {
-        console.log(key);
-    }
+for (let key in animal) {
+  if (animal.hasOwnProperty(key)) {
+    console.log(key);
+  }
 
 }
 
 // 装饰器多个 时候怎么执行
 @logger2()
 @logger1()
-class Logger{
+class Logger {
 
 }
-function logger1(){
-    console.log('outter 1')
-   return function(){
-        console.log('logger1');
-   }
-}   
-function logger2(){
-    console.log('outter 2')
-    return function(){
-        console.log('logger2');
-    }
+
+function logger1() {
+  console.log('outter 1')
+  return function () {
+    console.log('logger1');
+  }
 }
+
+function logger2() {
+  console.log('outter 2')
+  return function () {
+    console.log('logger2');
+  }
+}
+
 // 洋葱模型
 // 如果装饰器是函数 会直接执行，内部装饰类的时候会从内部依次传递到外部去
 
