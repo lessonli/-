25841fc296f6fe5c60df6e2036a71713c1fef267
lesson <lisen@qbyun.com>
2021-02-23 10:46:42 +0800

    冲突测试

--- javascript/prototypeChain/扩展内置原型.js
+++ javascript/prototypeChain/扩展内置原型.js
@@ -7,11 +7,13 @@ let arr = [1, 3, 4, 5, 2, 34, 5, 45, 6, 46, 743, 2, 342, 112, 3, 54, 345, 345656
 /* 先 去重 在 排序*/
 
 let result = arr.sort((a, b) => a - b);
+
 // console.log(result, arr); // 原始数组 改变 返回结果 也是 修改后的 新数组
 
 function unique(arr) {
-  return  [...new Set(arr)];
+  return [...new Set(arr)];
 }
+
 /* 扩展 原型 对象啊*/
 Array.prototype.unique = function unique() {
 //   this 指向当前类的实例
@@ -19,5 +21,5 @@ Array.prototype.unique = function unique() {
 }
 // console.log(unique(result));
 /* 先去重再排序*/
-let demo = arr.unique().sort(((a, b) => a-b));
+let demo = arr.unique().sort(((a, b) => a - b));
 console.log(demo)
