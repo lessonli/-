770de3105c538e367db17d694fb0d6e254686be9
lesson <lisen@qbyun.com>
2021-02-24 16:08:08 +0800

    ooc

--- javascript/OOC/12.js
+++ javascript/OOC/12.js
@@ -1,6 +1,14 @@
-/*
-*
-*
-*
-*
-* */
+~function(){
+  function change(){
+    //=>实现你的代码
+  };
+  Function.prototype.change=change;
+}();
+let obj = {name:'zhufeng'};
+function func(x,y){
+  this.total=x+y;
+  return this;
+}
+let res = func.change(obj,100,200);
+//res => {name:'Alibaba',total:300}
+console.log(res)
--- javascript/OOC/14.js
+++ javascript/OOC/14.js
@@ -1,5 +1,5 @@
-/*
-* var name = '珠峰培训';
+// 输出结果
+var name = 'lesson';
 function A(x,y){
     var res=x+y;
     console.log(res,this.name);
@@ -8,11 +8,11 @@ function B(x,y){
     var res=x-y;
     console.log(res,this.name);
 }
-B.call(A,40,30);
-B.call.call.call(A,20,10);
-Function.prototype.call(A,60,50);
-Function.prototype.call.call.call(A,80,70);
-* */
+B.call(A,40,30); // this 指向A   10,
+B.call.call.call(A,20,10); // 不懂什么玩意
+Function.prototype.call(A,60,50); // 不懂什么玩意儿
+Function.prototype.call.call.call(A,80,70);  // 不懂什么玩意
+
 
 
 
