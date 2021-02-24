ed9a0c5077c19e88c508aad3e2b2320cda15f4e2
lesson <1565510080@qq.com>
2021-02-24 22:21:04 +0800

    原型重定向

--- javascript/OOC/3.js
+++ javascript/OOC/3.js
@@ -12,8 +12,10 @@ Fn.prototype.b = function () {
 };
 
 console.log(f1.a); // 1
-console.log(f1.prototype); // undefined
+console.log(f1.prototype); // undefinedv  只有函数才具备 prototype (箭头函数以外)
 console.log(f1.b); // 函数体 ƒ () {this.a = 3; }
 console.log(f1.hasOwnProperty('b')); // false
 console.log('b' in f1); // true in 会 检测 原型链
-console.log(f1.constructor == Fn); // true
+console.log(f1.constructor ===  Fn); // true
+
+// 内置类的原型是无法重定向的
---
+++ javascript/OOC/原型重定向.js
@@ -0,0 +1,41 @@
+
+//  原型 重定向
+/*
+*     + 1. 内酯类的原型 无法重定向(不报错, 但是 也不成功) Object Array ...
+*     + 2. 在大量的向原型扩充方法的时候, 重定向的操作 一方面可以简化代码的编写，统一管理,
+* 弊端
+*     + 1. 重定向之后 丢失 自带的constructor
+*     + 2. 丢失 之前的原型上的方法（包含constructor）
+*
+* 解决方案:
+*     + 1. 如果之前的原型上没有任何的属性和方法, 则 重定向的原型对象手动设置一个constructor 即可
+*     + 2. 如果之前原型上还有其他的属性和方法, 则在重定向之前最好做 ‘新老’原型对象的合并处理
+* */
+
+function Fn() {
+  /* 两个私有属性*/
+  this.x = 100;
+  this.y = 200;
+}
+/* 会造成 代码的 冗余, 不好统一 管理*/
+Fn.prototype.read = function () {};
+Fn.prototype.write = function () {};
+// 。。。。 大量代码
+Fn.prototype.jump = function () {};
+
+/* 利用 原型重定向的方式来写*/
+
+/*Fn.prototype = {
+  /!* 什么都没有的情况*!/
+  constructor: Fn,
+
+}*/
+
+/* 原来的construcctor 也会携带*/
+Fn.prototype = Object.assign(Fn.prototype, {
+    say() {},
+    song(){},
+    eat(){},
+    jump(){}
+
+})
